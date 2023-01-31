f961aba39e0e0e48f2de0fe691860a6cb5054650
Long.Tran <tranhuuhoanglong248@gmail.com>
2023-01-31 11:40:42 +0700

    refactor config script alias hosts

--- scripts/alias_hosts.sh
+++ scripts/alias_hosts.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-hosts=(192.168.33.12 192.168.33.13 192.168.33.14 192.168.33.15)
-a=1
-for i in "${hosts[@]}"
-do
-    echo -e "$i centos${a}" 
-    a=$(expr $a + 1)
-done > /etc/hosts
+webs=(192.168.33.12 192.168.33.13)
+dbs=(192.168.33.14 192.168.33.15)
+
+alias_web_host(){
+    a=1
+    for i in "${webs[@]}"
+    do
+        echo -e "$i web${a}" 
+        a=$(expr $a + 1)
+    done >> /etc/hosts
+}
+
+alias_db_host(){
+    a=1
+    for i in "${dbs[@]}"
+    do
+        echo -e "$i db${a}" 
+        a=$(expr $a + 1)
+    done >> /etc/hosts
+}
+
+alias_web_host
+alias_db_host
